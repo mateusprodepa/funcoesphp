3b51d6a7ec8b1ee6bf4ec04da51e7918a57767e8
marcelo.cruz <marcelo.cruz@prodepa.pa.gov.br>
2018-06-15 13:56:55 -0300

    atualização

--- func.php
+++ func.php
@@ -1,31 +1,4 @@
 <?php
-// variáveis do Banco de Dados
-$host = "10.1.1.45";
-$usuario = "postgres";
-$senha = "prodepaalmenara2011";
-$banco = "seinfra_desenv";
-
-function testarBanco(){
-  global $host, $usuario, $senha, $banco;
-  $str  = "host=" . $host;
-  $str .= " port=5432";
-  $str .= " dbname=" . $banco;
-  $str .= " user=" . $usuario;
-  $str .= " password=" . $senha;
-
-  $conn = pg_connect($str);
-    if($conn) { echo "<strong style='color: #2ecc71;''><i>OK:</i></strong> Conexão com o banco funcionando normalmente"; }
-    else { echo "<strong style='color: #e74c3c;''><i>ERRO:</i></strong> Erro ao conectar com a base de dados"; }
-  }
-
-  function permissions($dir, $nome) {
-    if($dir) {
-      echo "<strong style='color: #2ecc71;''><i>OK:</i></strong> Permissão para a pasta <strong>\"$nome\"</strong> concedida e funcionando normalmente. <br>";
-    } else {
-      echo "<strong style='color: #f1c40f;''><i>AVISO:</i></strong> A pasta <strong>\"$nome\"</strong> não contém permissões de escrita. <br>";
-    }
-  }
-
   function testarPermissoes() {
 
     $tmpDir = __DIR__ . "/../php/tmp";
@@ -49,19 +22,63 @@ function testarBanco(){
 }
 
 function testarRedis() {
+  $dadosRedis = array(
+    "COMBO_TIPO_PROCEDIMENTO",
+    "PROCEDIMENTO_ATUACAO",
+    "COMBO_AUTORIDADE",
+    "COMBO_AUTORIDADE_USUARIO",
+    "COMBO_BOP_STATUS",
+    "COMBO_TIPO_PESSOA_ATUACAO",
+    "COMBO_CAUSA_PRESUMIVEL",
+    "COMBO_MEIO_EMPREGADO",
+    "COMBO_INSTRUMENTO_UTILIZADO",
+    "COMBO_UNIDADE",
+    "COMBO_ESCRIVAO",
+    "COMBO_LOCALOCORRENCIA",
+    "COMBO_GRAUPARENTESCO",
+    "COMBO_SEXO",
+    "COMBO_GRUPO_SOCIAL",
+    "COMBO_MORADIA",
+    "COMBO_PROFISSAO",
+    "COMBO_ESCOLARIDADE",
+    "COMBO_ESTADOCIVIL",
+    "COMBO_TIPODOCUMENTO",
+    "COMBO_TIPODOCUMENTO_F",
+    "COMBO_TIPODOCUMENTO_J",
+    "COMBO_PAIS",
+    "COMBO_UF",
+    "COMBO_LOCALIDADE",
+    "COMBO_BAIRRO",
+    "LISTA_TIPO_OBJETO",
+    "COMBO_CATEGORIA_OBJETO",
+    "COMBO_OBJETO",
+    "COMBO_OBJETOMARCA",
+    "COMBO_PROCEDIMENTO_STATUS",
+    "COMBO_TIPO_CONTATO",
+    "COMBO_PERFIL",
+    "COMBO_PERFIL_INTERNO",
+    "COMBO_CARACTERISTICA_SOMATICA_GRUPO",
+    "COMBO_CARACTERISTICA_SOMATICA",
+    "COMBO_MODUS_OPERANDI",
+    "COMBO_QUADRILHA",
+  );
+
   try {
     if(class_exists('Redis')) {
       if (!($redis = new Redis())) {
         throw new Exception("<strong style='color: #e74c3c;''><i>ERRO:</i></strong> Não foi possível testar o banco <strong>\"Redis\"</strong>. <br>");
       } else {
         if($redis->connect( 'localhost', 6379, 5 )) {
+          foreach ($dadosRedis as $key)
+            if($redis->exists($key))
+              echo "Arquivo REDIS $key gerado. <br>";
           echo "<strong style='color: #2ecc71;''><i>OK:</i></strong> O módulo <strong>\"Redis\"</strong> está instalado, e possível conectar ao banco. <br>";
         } else {
           echo "<strong style='color: #e74c3c;''><i>ERRO:</i></strong> O módulo <strong>\"Redis\"</strong> está instalado, mas não foi possível conectar ao banco <strong>\"Redis\"</strong>. <br>";
         }
       }
     } else {
-      echo "O módulo <strong>\"Redis\"</strong> não está instalado. <br>";
+      echo "<strong style='color: #e74c3c;''><i>ERRO:</i></strong> O módulo <strong>\"Redis\"</strong> não está instalado. <br>";
     }
   } catch(Exception $e) {
     echo $e;
@@ -69,13 +86,20 @@ function testarRedis() {
 }
 
 function modulosApache($arr) {
-  foreach ($arr as $key) {
-    if (apache_get_modules($key)) {
-      echo "<strong style='color: #2ecc71;''><i>OK:</i></strong> Modulo $key instalado corretamente.<br>";
-    } else {
-      echo "<strong style='color: #e74c3c;''><i>ERRO:</i></strong> O modulo <strong>\"$key\"</strong> não está instalado.";
-    }
+  $s = apache_get_modules($key);
+  foreach ($arr as $modulo) {
+      if(array_key_exists($modulo, $s)) {
+        echo "<strong style='color: #2ecc71;''><i>OK:</i></strong> O modulo $modulo está instalado corretamente.<br>";
+      } else {
+        echo "<strong style='color: #e74c3c;''><i>ERRO:</i></strong> O modulo Apache <strong>\"$modulo\"</strong> não está instalado. <br>";
+      }
   }
+}
+
+$modulosApache = array( "rewrite", "expires", "headers", "deflate", "auth" );
+function testarModulosApache() {
+  global $modulosApache;
+  modulosApache($modulosApache);
 }
 
 function json($nome) {
@@ -96,36 +120,16 @@ function verificarJson($arr) {
 }
 
 $arquivosJson = array(
-  "LISTA_TIPO_DESPACHO",
-  "DESPACHO_ACAO_OCORRENCIA",
-  "DESPACHO_ACAO_PROCEDIMENTO",
-  "COMBO_PERICIA_REGIONAL",
-  "COMBO_PERICIA_AREA_PERICIAL",
-  "COMBO_PERICIA_TIPO_EXAME",
-  "LISTA_TPL_CAMPO",
-  "BOP_CATEGORIA",
-  "COMBO_NATUREZA_BOP_CATEGORIA",
-  "COMBO_NATUREZA",
-  "COMBO_MEIO_EMPREGADO",
-  "COMBO_CAUSA_PRESUMIVEL",
-  "COMBO_GRUPO_OCORRENCIA",
-  "COMBO_LOCALOCORRENCIA",
-  "COMBO_UNIDADE",
-  "COMPO_BOP_STATUS",
-  "COMBO_PROCEDIMENTO_STATUS",
-  "COMPO_TIPO_PROCEDIMENTO",
-  "COMBO_PROCEDIMENTO_ORIGEM",
-  "COMBO_TIPO_PESSOA_ATUACAO",
-  "COMBO_ESTADOCIVIL",
-  "COMBO_ESCOLARIDADE",
-  "COMBO_PROFISSAO",
-  "COMBO_MORADIA",
-  "COMBO_GRUPO_SOCIAL",
-  "COMBO_GRUPO_SOCIAL",
-  "COMBO_SEXO",
-  "COMBO_MODUS_OPERANDI",
-  "COMBO_TIPO_CONTATO",
-  "COMBO_TIPODOCUMENTO"
+  "LISTA_TIPO_DESPACHO", "DESPACHO_ACAO_OCORRENCIA", "DESPACHO_ACAO_PROCEDIMENTO",
+  "COMBO_PERICIA_REGIONAL", "COMBO_PERICIA_AREA_PERICIAL", "COMBO_PERICIA_TIPO_EXAME",
+  "LISTA_TPL_CAMPO", "BOP_CATEGORIA", "COMBO_NATUREZA_BOP_CATEGORIA",
+  "COMBO_NATUREZA", "COMBO_MEIO_EMPREGADO", "COMBO_CAUSA_PRESUMIVEL",
+  "COMBO_GRUPO_OCORRENCIA", "COMBO_LOCALOCORRENCIA", "COMBO_UNIDADE",
+  "COMPO_BOP_STATUS", "COMBO_PROCEDIMENTO_STATUS", "COMPO_TIPO_PROCEDIMENTO",
+  "COMBO_PROCEDIMENTO_ORIGEM", "COMBO_TIPO_PESSOA_ATUACAO", "COMBO_ESTADOCIVIL",
+  "COMBO_ESCOLARIDADE", "COMBO_PROFISSAO", "COMBO_MORADIA",
+  "COMBO_GRUPO_SOCIAL", "COMBO_SEXO", "COMBO_MODUS_OPERANDI",
+  "COMBO_TIPO_CONTATO", "COMBO_TIPODOCUMENTO"
 );
 
 function testarJson() {
